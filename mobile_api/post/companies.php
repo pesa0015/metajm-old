e4e13461d9c82f756fc0b48bac4733d33f0aa5fa
Peter Sall <Peter@Peters-MBP.lan>
2015-12-27 23:07:50 +0100

    Initial commit

---
+++ mobile_api/post/companies.php
@@ -0,0 +1,35 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	require '../../mysql/query.php';
+
+	$lat = sqlEscape($_POST['lat']);
+	$lng = sqlEscape($_POST['lng']);
+
+	function checkUtf8($array) {
+		$new_array = array();
+
+		foreach($array as $item) {
+			$json = json_encode($item);
+			if (json_last_error() === JSON_ERROR_UTF8) {
+				$item['Bolagsnamn'] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($item['Bolagsnamn']));
+				$item['Postort'] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($item['Postort']));
+				$item['id'] = $item['id'];
+				$item['distance'] = $item['distance'];
+				array_push($new_array, $item);
+			}
+			else {
+				array_push($new_array, $item);
+			}
+		}
+		return $new_array;
+	}
+	$companies = sqlSelect("SELECT id, Bolagsnamn, Postort, ( 3959 * acos( cos( radians({$lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians({$lng}) ) + sin( radians({$lat}) ) * sin( radians( lat ) ) ) ) AS distance FROM companies HAVING distance < 1;");
+	echo json_encode(checkUtf8($companies));
+	
+}
+
+?>
---
+++ mysql/query.php
@@ -0,0 +1,42 @@
+<?php
+
+require 'connect.php';
+
+function sqlSelect($query) {
+	$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$stmt = $db->query($query);
+	$resultArray = array();
+	if ($stmt) {
+		while ($row = $stmt->fetch_assoc())
+			$resultArray[] = $row;
+	}
+
+	return $resultArray;
+
+	$stmt->closeCursor();
+	$db = null;
+}
+
+function sqlAction($query, $getLastId = false) {
+	$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	// $db->set_charset('utf8');
+	if ($db->query($query)) {
+		if ($getLastId)
+			return $db->insert_id;
+		return true;
+	}
+	return false;
+
+	$stmt->closeCursor();
+	$db = null;
+}
+
+function sqlEscape($string) {
+	$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+	$escaped_string = $db->real_escape_string($string);
+
+	return $escaped_string;
+}
+
+?>
