84a8f3d0e80cf91eb0b25164c7d40a3492697d09
Peter Sall <Peter@Peters-MBP.lan>
2016-01-13 17:21:32 +0100

    Get geolocation from new button

--- index.php
+++ index.php
@@ -20,13 +20,12 @@
         <form id="geolocation">
             <div id="position-area">
                 <input type="text" class="position" placeholder="Ange adress eller stad">
-                <input type="button" class="position" value="Hitta min position">
+                <input type="button" id="get-location" class="position" value="Hitta min position">
             </div>
             <button id="submit" class="ion-checkmark-round"></button>
         </form>
     </div>
 <?php if (!isset($_COOKIE['geolocation'])): ?>
-<button id="click">Hitta min position</button>
 <?php else: ?>
     <div id="map"></div>
 <?php endif; ?>
@@ -65,43 +64,46 @@
         // console.log(companies.length);
       }
       
-      document.getElementById('click').addEventListener('click', function() {
+      var getLocation = document.getElementById('get-location');
+      getLocation.addEventListener('click', function() {
             if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                // var baseUrl = 'http://46.101.90.3/mobile_api';
-                var baseUrl = 'http://localhost:8888/metajm';
-                function loadContent(e, lat, lng) {
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.onreadystatechange = function() {
-                        if (xhttp.readyState == 4 && xhttp.status == 200) {
-                            var companies = JSON.parse(xhttp.responseText);
-                            // var html = '';
-                            initialize(lat, lng, companies);
-                            for (var i = 0; i <= companies.length-1; i++) {
-                                // html += '<div class="company">' + companies[i].Bolagsnamn + '</div>';
-                                $(e).append('<div class="company">' + companies[i].Bolagsnamn + '</div>');
-                            };
-                            // document.getElementById(e).innerHTML = html;
-                            // console.log(companies);
+                getLocation.value = 'Söker..';
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    getLocation.value = 'Position hittad';
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    // var baseUrl = 'http://46.101.90.3/mobile_api';
+                    var baseUrl = 'http://localhost:8888/metajm';
+                    function loadContent(e, lat, lng) {
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                                var companies = JSON.parse(xhttp.responseText);
+                                // var html = '';
+                                initialize(lat, lng, companies);
+                                for (var i = 0; i <= companies.length-1; i++) {
+                                    // html += '<div class="company">' + companies[i].Bolagsnamn + '</div>';
+                                    $(e).append('<div class="company">' + companies[i].Bolagsnamn + '</div>');
+                                };
+                                // document.getElementById(e).innerHTML = html;
+                                // console.log(companies);
+                            }
                         }
+                        xhttp.open('POST', baseUrl + '/mobile_api/post/companies.php', true);
+                        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                        xhttp.send('lat=' + lat + '&lng=' + lng);
                     }
-                    xhttp.open('POST', baseUrl + '/mobile_api/post/companies.php', true);
-                    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                    xhttp.send('lat=' + lat + '&lng=' + lng);
-                }
-                loadContent($('#company-list'), pos.lat, pos.lng);
-                // console.log(pos);
-                // console.log(pos.lat);
-                // initialize(pos.lat, pos.lng);
-                $('html, body').animate({
-                    scrollTop: $('#map').offset().top
-                }, 2000);
-            })
-        }
+                    loadContent($('#company-list'), pos.lat, pos.lng);
+                    // console.log(pos);
+                    // console.log(pos.lat);
+                    // initialize(pos.lat, pos.lng);
+                    $('html, body').animate({
+                        scrollTop: $('#map').offset().top
+                    }, 2000);
+                })
+            }
         });
         // $(document).ready(function (){
         //     $('#click').click(function (){
