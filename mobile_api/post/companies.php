34bba5b9a357d5defa39ce1710ebb3ed66744d8d
Peter Sall <Peter@Peters-MBP.lan>
2016-01-14 19:09:11 +0100

    Fetch more data

--- mobile_api/post/companies.php
+++ mobile_api/post/companies.php
@@ -16,6 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$json = json_encode($item);
 			if (json_last_error() === JSON_ERROR_UTF8) {
 				$item['Bolagsnamn'] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($item['Bolagsnamn']));
+				$item['Adress'] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($item['Adress']));
+				$item['Postnr'] = $item['Postnr'];
 				$item['Postort'] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($item['Postort']));
 				$item['id'] = $item['id'];
 				$item['lat'] = $item['lat'];
@@ -29,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		}
 		return $new_array;
 	}
-	$companies = sqlSelect("SELECT id, Bolagsnamn, Postort, lat, lng, ( 3959 * acos( cos( radians({$lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians({$lng}) ) + sin( radians({$lat}) ) * sin( radians( lat ) ) ) ) AS distance FROM companies HAVING distance < 1;");
+	$companies = sqlSelect("SELECT id, Bolagsnamn, Adress, Postnr, Postort, lat, lng, ( 3959 * acos( cos( radians({$lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians({$lng}) ) + sin( radians({$lat}) ) * sin( radians( lat ) ) ) ) AS distance FROM companies HAVING distance < 1;");
 	echo json_encode(checkUtf8($companies));
 	
 }
