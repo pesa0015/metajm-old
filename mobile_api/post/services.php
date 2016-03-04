fa8263dc21a46f1891192e3ca0778e496b5d44fe
Peter Sall <Peter@Peters-MBP.lan>
2016-03-04 15:36:49 +0100

    Access-Control-Allow-Origin

--- mobile_api/post/services.php
+++ mobile_api/post/services.php
@@ -2,6 +2,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (isset($_POST['company_id']) && is_numeric($_POST['company_id'])) {
+		header('Access-Control-Allow-Origin: *');
 		header('Content-Type: text/html;charset=utf-8');
 		require '../../mysql/query.php';
 		$s = sqlSelect("SELECT services.id, services.name AS description, price, time, category.name AS category_name FROM `companies_employers_services` INNER JOIN companies_employers INNER JOIN services INNER JOIN category ON companies_employers_services.service_id = services.id AND services.category_id = category.id WHERE companies_employers.company_id = {$_POST['company_id']} GROUP BY services.id ORDER BY services.category_id;");
