678e4de0e2915c3cdae5e6e1968c235d47852949
Peter Sall <Peter@Peters-MBP.lan>
2016-03-14 18:49:22 +0100

    Simplify, more cleaner

--- js/company.services.js
+++ js/company.services.js
@@ -83,28 +83,8 @@ $('#select2').select2({
 	  			width: '100%',
 	  			data: data
 	  		});
-	  		// var preload_data = [{ id: 1, text: 'Disabled User'}];
-	  		// $(existingServices[i]).css('width', '300px');
-	  		// var data = [{id: "CA", text: "California"},{id:"MA", text: "Massachusetts"}];
-	  		// insert(existingServices[i].value, existingServices[i].id);
-// 	  		var data = [{
-//     id: 1,
-//     text: "A"
-// }, {
-//     id: 2,
-//     text: "B"
-// }, {
-//     id: 3,
-//     text: "C"
-// }];
-		// console.log(existingServices[i].id);
-		$(existingServices[i]).select2({width: '100%', tags: true, maximumSelectionSize: 1, data: [{id: existingServices[i].value, text: existingServices[i].id}]});
+	  	$(existingServices[i]).select2({width: '100%', tags: true, maximumSelectionSize: 1, data: [{id: existingServices[i].value, text: existingServices[i].id}]});
 		$(existingServices[i]).on('select2-removed', function(e) {
-			// e.val = 'färg';
-			
-			// console.log(e.target.id);
-			// addSelect2($(existingServices[i]), false);
-			// console.log(e);
 			$(e.target).select2({
 				width: '100%',
 				tokenSeparators: [","],
@@ -145,9 +125,7 @@ $('#select2').select2({
 				        }
 				    }
 				});
-		});
-	  		// $(existingServices[i]).select2({data: data});
-	  		// console.log($(existingServices)[i]);
+			});
 	  	}
 	  	// var category = document.getElementsByClassName('new_service select2-false')[0];
 	  	// 	var time = document.getElementsByClassName('new_service select2-false')[4];
@@ -164,16 +142,11 @@ $('#select2').select2({
 		newService.addEventListener('click', function() {
 			var row = serviceTable.insertRow(rowNr);
 			rowNr--;
-			var category = row.insertCell(0).innerHTML = '<input type="text" id="category-' + rowNr + '" class="new-service" name="new_service[' + rowNr + '][\'category\']">';
-			var description = row.insertCell(1).innerHTML = '<input type="text" id="description-' + rowNr + '" class="new-service" name="new_service[' + rowNr + '][\'description\']">';
-			var price = row.insertCell(2).innerHTML = '<input type="text" id="price-' + rowNr + '" class="new-service" name="new_service[' + rowNr + '][\'price\']">';
-			var time = row.insertCell(3).innerHTML = '<input type="text" id="time-' + rowNr + '" class="new-service" name="new_service[' + rowNr + '][\'time\']">';
+			var category = row.insertCell(0).innerHTML = '<input type="text" id="category-' + rowNr + '" class="new-service category" name="new_service[' + rowNr + '][\'category\']">';
+			var description = row.insertCell(1).innerHTML = '<input type="text" id="description-' + rowNr + '" class="new-service description" name="new_service[' + rowNr + '][\'description\']">';
+			var price = row.insertCell(2).innerHTML = '<input type="text" id="price-' + rowNr + '" class="new-service price" name="new_service[' + rowNr + '][\'price\']">';
+			var time = row.insertCell(3).innerHTML = '<input type="text" id="time-' + rowNr + '" class="new-service time" name="new_service[' + rowNr + '][\'time\']">';
 			addSelect2($('#category-' + rowNr), $('#time-' + rowNr));
 			rowNr++;
 			rowNr++;
-			// var td = '<td><input type="text" class="new_service select2-false" name="new_service[]"></td>';
-			// var input = '<tr>' + td + td + td + td + td + '</tr>';
-			// document.getElementById('service').innerHTML += input;
-			// addSelect2(document.getElementsByClassName('new-service select2-false'));
-			// addSelect2($('#category-' + rowNr), $('#time-' + rowNr));
 		});
--- views/company/services.php
+++ views/company/services.php
@@ -1,40 +1,6 @@
 <?php
 $admin = sqlSelect("SELECT admin FROM companies_employers WHERE id = {$_SESSION['me']['id']} AND company_id = {$_SESSION['company']['id']}");
 if ($admin[0]['admin'] == 1): ?>
-<form action="mobile_api/post/add_service" method="post" id="manage-services">
-		<h1>Redigera tjänster</h1>
-		<table id="service">
-			<tr>
-				<th>Kategori</th>
-				<th>Beskrivning</th>
-				<th>Pris (sek)</th>
-				<th>Tid (h)</th>
-			</tr>
-			<?php
-			$services = sqlSelect("SELECT services.id, services.name AS service_name, price, time, category.id AS category_id, category.name AS category_name FROM `services` INNER JOIN category ON category.id = services.category_id WHERE company_id = {$_SESSION['company']['id']} ORDER BY id;");
-			$i = 0;
-			if ($services) {
-				foreach ($services as $service) { ?>
-				<tr>
-					<input type="hidden" name="existing_service[<?=$i; ?>]['service_id']" value="<?=$service['id']; ?>">
-					<td><input type="text" id="<?=$service['category_name']; ?>" class="existing-service category" name="existing_service[<?=$i; ?>]['category']" value="<?=$service['category_id']; ?>"></td>
-					<td><input type="text" class="existing-service" name="existing_service[<?=$i; ?>]['description']" value="<?=$service['service_name']; ?>"></td>
-					<td><input type="text" class="existing-service" name="existing_service[<?=$i; ?>]['price']" value="<?=$service['price']; ?>"></td>
-					<td><input type="text" class="existing-service time" name="existing_service[<?=$i; ?>]['time']" value="<?=$service['time']; ?>"></td>
-					<td><a href="mobile_api/post/remove_service?service_id=<?=$service['id']; ?>">Ta bort</a></td>
-				</tr>
-				<?php $i++; }} ?>
-			<tr>
-				<!-- <td><input type="text" id="category-1" class="new-service" name="new_service[<?=$i; ?>]['category']"></td>
-				<td><input type="text" id="description-1" class="new-service" name="new_service[<?=$i; ?>]['description']"></td>
-				<td><input type="text" id="price-1" class="new-service" name="new_service[<?=$i; ?>]['price']"></td>
-				<td><input type="text" id="time-1" class="new-service" name="new_service[<?=$i; ?>]['time']"></td> -->
-			</tr>
-		</table>
-		<input type="button" id="add-new-service" value="+">
-		<input type="submit" value="Uppdatera">
-	</form>
-	<script>var rowNr = <?=count($services); ?>+2;</script>
 <?php endif; ?>
 	<div id="existing_services">
 		<h1>Nuvarande tjänster</h1>
@@ -49,7 +15,7 @@ if ($admin[0]['admin'] == 1): ?>
 	}
 		// $services = sqlSelect("SELECT services.id, services.name, services.price, services.time, services.category_id FROM services INNER JOIN companies_services ON services.id = companies_services.service_id WHERE companies_services.company_id = {$_SESSION['company_id']};");
 		if ($services) { ?>
-			<table>
+			<table id="service">
 				<tr>
 					<th>Kategori</th>
 					<th>Beskrivning</th>
@@ -59,6 +25,7 @@ if ($admin[0]['admin'] == 1): ?>
 			<?php
 			foreach ($services as $service) {
 				$category = sqlSelect("SELECT category.id, category.name FROM category INNER JOIN services ON category.id = services.category_id WHERE services.category_id = {$service['category_id']};");
+				$i = 0;
 				?>
 				<tr>
 					<td><?=$service['category_name']; ?></td>
@@ -75,34 +42,32 @@ if ($admin[0]['admin'] == 1): ?>
 						<td><a href="mobile_api/post/add_my_service.php?service_id=<?=$service['id']; ?>">Använd</a></td>
 					<?php } ?>
 				</tr>
-				<?php } ?>
+				<?php $i++; } ?>
+				<script>var rowNr = <?=count($services); ?>+1;</script>
 			</table>
 			<?php } else { ?>
 			<p>Ni har inga tjänster.</p>
 		<?php } ?>
-		</div>
-		<div id="my_services">
-			<h1>Mina tjänster</h1>
-			<?php
-			if ($my_services) { ?>
-				<table>
-					<tr>
-						<th>Kategori</th>
-						<th>Beskrivning</th>
-						<th>Pris (sek)</th>
-						<th>Tid (h)</th>
-					</tr>
-					<?php foreach($my_services as $my_service) { ?>
-					<tr>
-						<td><?=$my_service['category_name']; ?></td>
-						<td><?=$my_service['service_name']; ?></td>
-						<td><?=$my_service['price']; ?></td>
-						<td><?=$my_service['time']; ?></td>
-						<td><a href="mobile_api/post/remove_my_service.php?service_id=<?=$my_service['id']; ?>">Ta bort</a></td>
-					</tr>
-					<?php }
-			}
-			else { ?>
-				<p>Du har inga tjänster.</p>
-			<?php } ?>
+			<div>
+				<span id="add-new-service" class="add-new-service">
+					<i class="ion-ios-plus-outline service"></i>
+					<i class="ion-ios-plus service"></i>
+					<span>Lägg till tjänst</span>
+				</span>
+				<script>
+				// document.getElementById('add-new-service').addEventListener('mouseover',function(){
+				// 	document.getElementsByClassName('ion-ios-plus-outline service')[0].style.display = 'none';
+				// 	document.getElementsByClassName('ion-ios-plus service')[0].style.display = 'inline-block';
+				// });
+				// document.getElementById('add-new-service').addEventListener('mouseout',function(){
+				// 	document.getElementsByClassName('ion-ios-plus service')[0].style.display = 'none';
+				// 	document.getElementsByClassName('ion-ios-plus-outline service')[0].style.display = 'inline-block';
+				// });
+				</script>
+				<button type="submit" class="add-new-service">
+					<i class="ion-ios-checkmark-outline service"></i>
+					<i class="ion-ios-checkmark service"></i>
+					<span>Uppdatera</span>
+				</button>
+			</div>
 		</div>
