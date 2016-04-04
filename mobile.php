b75f3b862e8c295bfa01fec77067f3b540a17eca
Peter Sall <Peter@Peters-MBP.lan>
2016-04-04 20:04:34 +0200

    Redirect when mobile

---
+++ mobile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<title>Mitajm</title>
+	<link rel="stylesheet" type="text/css" href="vendor/font-awesome.min.css">
+	<style>
+		@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600);
+		body { font-family: 'Source Sans Pro', sans-serif; margin: 0; }
+		#middle { width: 100%; height: 90px; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; }
+		#middle div { text-align: center; }
+		#logo { font-size: 70px; }
+		#text { font-size: 25px; }
+		i.fa.fa-hand-o-right { font-size: 25px; display: block; text-align: center; margin-top: 10px;}
+	</style>
+</head>
+<body>
+	<div id="middle">
+		<div id="logo">Mitajm</div>
+		<div id="text">Snart i mobilen</div>
+		<i class="fa fa-hand-o-right"></i>
+	</div>
+</body>
+</html>
